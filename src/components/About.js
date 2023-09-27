728a75582fced145c0390ff03a3a6af8e3fed18e
spittam <85659804+spittam@users.noreply.github.com>
2023-09-26 20:05:04 -0700

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-import OrderList from './OrderList';
-
 function About() {
   return (
-    <div>
-      <h2>About Page</h2>
-      <p>Learn more about our SPA here.</p>
-    </div>
+<div class="container">
+ 
+ <form action="https://localemv.com:8887" method="POST">
+     <div class="col-md-6 col-md-offset-3">
+             <h1>Sample Payment (Redirect)</h1>
+<input type="hidden" name="xCommand" value="cc:sale" />
+<label for="uname"><b>Amount</b></label>
+<input type="text" name="xAmount" value="1.99" />
+
+
+
+
+<label for="psw"><b>Invoice</b></label>
+<input type="text" name="xInvoice" value="12345" /> 
+<input type="hidden" name="xStreet" value="123 Main St" />
+<input type="hidden" name="xZip" value="11111" />
+   
+<input type="hidden" name="xRedirectURL" value="https://localhost.3000/paymentrespone" />
+<input type="submit" value="Initiate Payment" />
+
+</div>
+</form>
+</div>
   );
 }
 
