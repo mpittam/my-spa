58f35cf1557adaa80f60b35aab09cd9e9d09ec29
Nimish47 <nimishnishant1995@gmail.com>
2023-10-06 09:26:46 +0530

    Update woocommerce sample creds

--- src/config/woocommerce/secret.js
+++ src/config/woocommerce/secret.js
@@ -1,3 +1,3 @@
-export const STORE_URL = '';
-export const CONSUMER_KEY = '';
-export const CONSUMER_SECRET = '';
+export const STORE_URL = 'https://woo-casually-groovy-nacho.wpcomstaging.com/wp-json/wc/v3';
+export const CONSUMER_KEY = 'ck_1e27096a6a801dddc9d44eaa2f7ef6e277a45fbe';
+export const CONSUMER_SECRET = 'cs_22be3163b221b9349c795455592dd1af099d8154';
